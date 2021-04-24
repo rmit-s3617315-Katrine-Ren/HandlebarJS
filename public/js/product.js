791ca426ca26de39665116b2e7334e4257ba7ba8
Katrine Ren <s3617315@student.rmit.edu.au>
2021-04-24 15:12:16 +1000

    polish the display of product page

---
+++ public/js/product.js
@@ -0,0 +1,33 @@
+
+let productImg = document.getElementById('productImg');
+
+const smallImgs = document.querySelectorAll('.extra-img');
+
+smallImgs[0].onclick = () => {
+    productImg.src = smallImgs[0].src;
+}
+
+smallImgs[1].onclick = () => {
+    productImg.src = smallImgs[1].src;
+}
+
+smallImgs[2].onclick = () => {
+    productImg.src = smallImgs[2].src;
+}
+
+smallImgs[3].onclick = () => {
+    productImg.src = smallImgs[3].src;
+}
+
+smallImgs[4].onclick = () => {
+    productImg.src = smallImgs[4].src;
+}
+smallImgs[5].onclick = () => {
+    productImg.src = smallImgs[5].src;
+}
+smallImgs[6].onclick = () => {
+    productImg.src = smallImgs[6].src;
+}
+smallImgs[7].onclick = () => {
+    productImg.src = smallImgs[7].src;
+}
